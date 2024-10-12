2824fb78c13d76d7bed1981a76f69dc46ae00228
sickk <jaakmyr@gmail.com>
2024-10-12 14:39:57 -0700

    added prawn cursor and lawn background

--- src/assets/Components/FlashingBg.jsx
+++ src/assets/Components/FlashingBg.jsx
@@ -5,7 +5,7 @@ import '../css/flashingbg.css'
 function FlashingBg({ flashSpeed }) {
     const [backgroundColor, setBackgroundColor] = useState(null);
     const [borderColor, setBorderColor] = useState(null);
-    const getRandomColor = () => '#' + Math.floor(Math.random()*16777215).toString(16);
+    const getRandomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
 
     useEffect(() => {
         setInterval(() => {
--- src/assets/Components/FlyingPrawn.jsx
+++ src/assets/Components/FlyingPrawn.jsx
@@ -10,6 +10,7 @@ function FlyingPrawn({ prawnCount }) {
                 speed: Math.random()*5 + 2 + "s",
                 scale: Math.random()*60 + 20 + "%",
                 height: Math.random() * 100 + "%",
+                resetTime: Math.random() * 3 + "s",
                 direction: Math.random() > 0.5 ? 1 : -1,
                 id: i,
                 })
@@ -26,6 +27,7 @@ function FlyingPrawn({ prawnCount }) {
             '--speed': obj.speed,
             '--height': obj.height,
             '--direction': obj.direction,
+            '--reset-time': obj.resetTime,
             transform: `translateY(-50%) scale(${obj.scale}) scaleX(${obj.direction})`,
         }}></img>
     ))
